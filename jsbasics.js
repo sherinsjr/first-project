1b2db6ee56febf73c001ef6dac6654c2f840a8dd
SHerin Reji <sherinrobert1952@gmail.com>
2023-01-05 14:23:21 +0530

    first change

---
+++ jsbasics.js
@@ -0,0 +1,57 @@
+//? OPERATORS..
+//? 1 arithametic opertaors..
+
+//!addtion
+// function add(a, b) {
+//   var s = a + b;
+//   console.log(s);
+// }
+// add(6, 1);
+
+//!subtration
+// function sub(a, b) {
+//   var s = a - b;
+//   console.log(s);
+// }
+// sub(6, 1);
+
+//! multiplication
+// function multi(a, b) {
+//   var s = a * b;
+//   console.log(s);
+// }
+// multi(6, 1);
+
+//! division
+// function division(a, b) {
+//   var s = a / b;
+//   console.log(s);
+// }
+// division(6, 2);
+
+//?class work
+// function factorial(n) {
+//   let factorial = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1;
+//   console.log(factorial);
+// }
+// factorial(10);
+
+// function convertFahrenheit(celsius) {
+//   let C = celsius;
+//   let result = [(C * 9) / 5] + 32;
+//   console.log("The fahrenheit value is:", result);
+// }
+// function convertCelsius(fahrenheit) {
+//   let F = fahrenheit;
+//   let result = [(F - 32) * 5] / 9;
+//   console.log("The celcius value is:", result);
+// }
+// convertFahrenheit(100);
+// convertCelsius(100);
+
+//? Area of rectangle
+// function area(l, b) {
+//   let area = l * b;
+//   console.log(area);
+// }
+// area(5, 6);
